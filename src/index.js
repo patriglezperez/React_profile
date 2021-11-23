38ebcbfaa44a5aad282b35e300e2d3caa90d23ce
aldo-castellano <92597205+aldo-castellano@users.noreply.github.com>
2021-11-23 22:01:16 +0100

    add botton edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-
+// import img from '../assets/icons.svg';
 let basicInfo = { fullname: 'Cathy Chu', user: 'cchu01', source: 'https://cdn-images.livecareer.es/pages/foto_cv_lc_es_7.jpg', email: 'cchu@myschool.edu', id: 'Add Student ID', password: 'Change password', background: 'https://static.vecteezy.com/system/resources/previews/001/888/309/non_2x/light-green-yellow-modern-blurred-backdrop-vector.jpg' }
 
 let systemSetting = { language: 'English (United States)', privacy: 'Only adminitrators and other instructors can view my profile information', notifications: ['Stream notifications', 'Email notifications', 'Push notifications'] }
@@ -11,51 +11,72 @@ let additionalInfo = { gender: 'Add gender' }
 let information1 = {
   title: "Basic Information",
   elements: [
-    { textname: "Full Name", value: basicInfo.fullname, isEditable: true },
-    { textname: "Email Adress", value: basicInfo.email },
-    { textname: "Student ID", value: basicInfo.id },
-    { textname: "Password", value: basicInfo.password },
+    { id: 1, textname: "Full Name", value: basicInfo.fullname, isEditable: true },
+    { id: 2, textname: "Email Adress", value: basicInfo.email },
+    { id: 3, textname: "Student ID", value: basicInfo.id },
+    { id: 4, textname: "Password", value: basicInfo.password },
   ]
 }
 
 let information2 = {
   title: "System Settings",
   elements: [
-    { textname: "Language", value: systemSetting.language },
-    { textname: "Privacy Settings", value: systemSetting.privacy },
-    { textname: "Global Notifications Settings", array: true, array0: systemSetting.notifications[0], array1: systemSetting.notifications[1], array2: systemSetting.notifications[2] },
+    { id: 1, textname: "Language", value: systemSetting.language },
+    { id: 2, textname: "Privacy Settings", value: systemSetting.privacy },
+    { id: 3, textname: "Global Notifications Settings", value: systemSetting.notifications },
   ]
 }
 
 let information3 = {
   title: "Aditional information",
-  elements: [{ textname: "Gender", value: additionalInfo.gender }]
+  elements: [{ id: 1, textname: "Gender", value: additionalInfo.gender }]
 }
 class Item extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { id: props.id, textname: props.textname, value: props.value, noIsEditable: true, isEditable: props.isEditable }
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleClick() {
+    this.setState(state => ({ noIsEditable: !state.noIsEditable }))
+
+  }
+  handleChange(event) {
+    this.setState(() => ({ value: event.target.value }))
+
+  }
   render() {
-    const array = this.props.array
-    if (array === true) {
+    if (Array.isArray(this.props.value)) {
       return <>
         <div className='item_array'>
           <p className="text" >{this.props.textname}</p>
           <div className='div_array'>
-            <p className="array">{this.props.array0}</p>
-            <p className="array">{this.props.array1}</p>
-            <p className="array">{this.props.array2}</p>
+            {this.props.value.map(element => { return <p key={element.id} className="array">{element}</p> })}
+
           </div>
         </div>
       </>
     } else {
       return <>
         <div className='item'>
-          <p className="text" >{this.props.textname}</p>
-          <p className="value">{this.props.value}</p>
+          <p className="text" >{this.state.textname}</p>
+          <div>
+            {this.state.noIsEditable ?
+              <p className="value">{this.state.value}</p>
+              :
+              <input type='text' onChange={this.handleChange} />
+            }
+            {this.state.isEditable ? <img src='./assets/icons.svg' alt="edit" className='edit' onClick={this.handleClick} /> : null}
+          </div>
+
         </div>
       </>
+      // }
     }
   }
 }
-
 class Info extends React.Component {
   render() {
 
@@ -63,7 +84,9 @@ class Info extends React.Component {
     return <div className='section'>
       <p className='title-section'>{this.props.title}</p>
       {this.props.data.map(element => {
-        return <Item textname={element.textname} value={element.value} array={element.array} array0={element.array0} array1={element.array1} array2={element.array2} />
+
+        return <Item key={element.id} id={element.id} textname={element.textname} value={element.value} isEditable={element.isEditable} />
+
       })}
     </div>
   }
@@ -83,8 +106,8 @@ class Profile extends React.Component {
       <div className="container-my-info">
         <a href="/"></a>
         <img className="pic-profile" src={this.props.user.source} alt="picture" />
-        <p class='fullname'>{this.props.user.fullname}</p>
-        <p class='username'>{this.props.user.user}</p>
+        <p className='fullname'>{this.props.user.fullname}</p>
+        <p className='username'>{this.props.user.user}</p>
       </div>
       <div className='page-distribution'>
         <div className='distribution-left' >
